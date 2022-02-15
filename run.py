3eac6881fa7a40a263b54692b3e35670ce891fdc
dontic <dontic@zoho.com>
2022-02-15 22:47:03 +0100

    Added __name__ condition

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 from app import create_app
-app = create_app()
-app.run(host='0.0.0.0', port=5012, debug=True)
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5012, debug=True)
