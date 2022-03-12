acf2a3e34c6c833d3f095ea2b3936a38f1f7a80b
dontic <dontic@zoho.com>
2022-03-12 19:03:12 +0100

    Deleted local config variables in favour of user-dependent variables

--- config.py
+++ config.py
@@ -3,35 +3,16 @@ class Config(object):
     # Statement for enabling new signups
     ENABLE_SIGNUPS = True
 
-    # Number of database backup versions after cleanup
+    # Maximum number of database backup versions after cleanup
     N_BACKUPS = 2
 
 
     ###### PATH MAP ######
-    # Default time span for path map plot in days (days back from now)
-    DAYS_PERIOD_PATH = 7
-
     # Max number of days to query in path map
-    MAX_QUERY_DAYS_PATH = 30
-
-    # Max accuracy value for path map points, default = 10 meters
-    # This will delete any points that have accuracy > MAX_ACCURACY_PATH
-    MAX_ACCURACY_PATH = 10
-
-    # Reduce points
-    REMOVE_IDLE = True
-
-    # Plot trips as colors
-    PLOT_TRIP_COLOR = True
+    MAX_QUERY_DAYS_PATH = 15
 
 
     ###### VISITS MAP ######
-    # Ignore home location in visits map by default
-    IGNORE_HOME = True
-
-    # Default time span for map plots in days (days back to now)
-    DAYS_PERIOD_VISITS = 7
-
     # Max number of days to query in visits map
     MAX_QUERY_DAYS_VISITS = 30
 
