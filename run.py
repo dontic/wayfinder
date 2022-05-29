ab481874d9edc45a15488023b50893db74753ada
dontic <dontic@zoho.com>
2022-05-29 12:15:20 +0200

    Automated creating users sqlite3 database

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+import pwd
 from app import create_app
 app = create_app()
+
+# Create SQLITE3 users database if it doesn't exist
+from pathlib import Path
+if not Path(Path.cwd()/'app'/'users.db').is_file():
+    from app.extensions import db
+    from app.auth.models import *
+    db.create_all(app=app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5012, debug=True)
