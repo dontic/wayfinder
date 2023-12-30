ebd1ddf27e77b9a970c601547bc32fdb506790c7
Daniel Garcia Sanchez <dontic@zoho.com>
2023-12-30 23:54:15 +0100

    Modify static config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -195,7 +195,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = FORCE_SCRIPT_NAME + "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# STATIC_URL = FORCE_SCRIPT_NAME + "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
