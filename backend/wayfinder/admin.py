6308ee2aa82c15e0da3e6073d2f6aed52d750a9f
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-13 15:10:01 +0000

    Add admin models for Location and Visit

--- backend/wayfinder/admin.py
+++ backend/wayfinder/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Location, Visit
 
 # Register your models here.
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "time",
+        "longitude",
+        "latitude",
+        "altitude",
+    )
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = (
+        "arrival_date",
+        "departure_date",
+        "latitude",
+        "longitude",
+    )
