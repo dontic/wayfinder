7db6eff5435c97847dc5b2aac4ef95f471d1d3c6
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-19 17:23:12 +0000

    Create login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <NotFound />
+  }
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>
 );
