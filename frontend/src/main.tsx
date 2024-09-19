6a3f3deade2833853bc59854ee7e83f8854968e6
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-19 15:26:41 +0000

    Refactor main.tsx to use ChakraProvider for styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
