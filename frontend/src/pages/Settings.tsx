6ff1c6da3a20486d0543e6f2e440aa7ede5c6b8a
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-21 14:28:07 +0000

    frontend: Update Settings layout

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,49 +1,35 @@
-import { Box, Divider, Text, VStack } from "@chakra-ui/react";
+import { Box, Text, VStack } from "@chakra-ui/react";
 import OverlandToken from "~/components/OverlandToken";
 import PasswordChange from "~/components/PasswordChange";
 import UsernameChange from "~/components/UsernameChange";
 
-import CenteredLayout from "~/layouts/CenteredLayout";
-
 const Settings = () => {
   /* --------------------------------- RENDER --------------------------------- */
   return (
-    <CenteredLayout>
-      <Box
-        w={{ base: "100%", md: "70%" }}
-        h={{ base: "100vh", md: "auto" }}
-        bg={"white"}
-        rounded={"lg"}
-        boxShadow={"lg"}
-        p={{ base: 12, md: 8 }}
-      >
-        <VStack spacing={4} w={"100%"} pt={{ base: 4, md: 0 }}>
-          <Box>
-            <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
-              Overland Token
-            </Text>
-            <OverlandToken />
-          </Box>
+    <Box w="100%" h={"100vh"} overflowY={"scroll"}>
+      <VStack spacing={10} py={{ base: 4, md: 10 }}>
+        <Box bg={"white"} rounded={"md"} boxShadow={"md"} p={4}>
+          <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
+            Overland Token
+          </Text>
+          <OverlandToken />
+        </Box>
 
-          <Divider />
-          <Box>
-            <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
-              Change Password
-            </Text>
-            <PasswordChange />
-          </Box>
+        <Box bg={"white"} rounded={"md"} boxShadow={"md"} p={4}>
+          <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
+            Change Password
+          </Text>
+          <PasswordChange />
+        </Box>
 
-          <Divider />
-
-          <Box>
-            <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
-              Change Username
-            </Text>
-            <UsernameChange />
-          </Box>
-        </VStack>
-      </Box>
-    </CenteredLayout>
+        <Box bg={"white"} rounded={"md"} boxShadow={"md"} p={4}>
+          <Text mb={3} fontSize="xl" fontWeight="bold" textAlign={"center"}>
+            Change Username
+          </Text>
+          <UsernameChange />
+        </Box>
+      </VStack>
+    </Box>
   );
 };
 
